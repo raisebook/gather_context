47ba98e0d8d34c20d5c842c70ccdabdcdf0a26b3
Myles Eftos <myles@madpilot.com.au>
2018-05-24 14:57:58 +1000

    Refactoring account spec to use shared examples

--- spec/lib/gather_context/api/account_spec.exs
+++ spec/lib/gather_context/api/account_spec.exs
@@ -1,5 +1,26 @@
+defmodule SharedAccount do
+  use ESpec, shared: true
+
+  it "which has an id" do
+    expect(shared.subject().id) |> to(eq(shared.id))
+  end
+
+  it "which has a name" do
+    expect(shared.subject().name) |> to(eq(shared.name))
+  end
+
+  it "which has a slug" do
+    expect(shared.subject().slug) |> to(eq(shared.slug))
+  end
+
+  it "which has a timezone" do
+    expect(shared.subject().timezone) |> to(eq(shared.timezone))
+  end
+end
+
 defmodule GatherContext.API.AccountSpec do
   use ESpec
+  alias GatherContext.API.{Client, Account}
 
   let :obj, do: %{
     "id" => "123456",
@@ -8,59 +29,35 @@ defmodule GatherContext.API.AccountSpec do
     "timezone" => "UTC"
   }
 
+  before subject: subject(),
+          id: 123456,
+          name: "Example",
+          slug: "example",
+          timezone: "UTC"
+
   describe "all" do
     let :response, do: {:ok, [obj()]}
-    let :client, do: %GatherContext.API.Client{get: fn(_) -> response() end}
-    subject do: (GatherContext.API.Account.all(client()) |> elem(1))
+    let :client, do: %Client{get: fn(_) -> response() end}
+    subject do: (Account.all(client()) |> elem(1))
 
     it "returns a List" do
       expect(subject() |> length) |> to(eq(1))
     end
 
     describe "builds a List of %GatherContext.API.Account objects" do
-      subject do: (GatherContext.API.Account.all(client()) |> elem(1) |> List.first)
-
-      it "which has an id" do
-        expect(subject().id) |> to(eq(123456))
-      end
-
-      it "which has a name" do
-        expect(subject().name) |> to(eq("Example"))
-      end
-
-      it "which has a slug" do
-        expect(subject().slug) |> to(eq("example"))
-      end
-
-      it "which has a timezone" do
-        expect(subject().timezone) |> to(eq("UTC"))
-      end
+      subject do: (Account.all(client()) |> elem(1) |> List.first)
+      it_behaves_like(SharedAccount)
     end
   end
 
   describe "get_account" do
     let :response, do: {:ok, obj()}
-    let :client, do: %GatherContext.API.Client{get: fn(_) -> response() end}
-    subject do: (GatherContext.API.Account.get_account(client(), 123456) |> elem(1))
+    let :client, do: %Client{get: fn(_) -> response() end}
+    subject do: (Account.get_account(client(), 123456) |> elem(1))
 
     describe "builds a %GatherContext.API.Account object" do
-      subject do: (GatherContext.API.Account.get_account(client(), 123456) |> elem(1))
-
-      it "which has an id" do
-        expect(subject().id) |> to(eq(123456))
-      end
-
-      it "which has a name" do
-        expect(subject().name) |> to(eq("Example"))
-      end
-
-      it "which has a slug" do
-        expect(subject().slug) |> to(eq("example"))
-      end
-
-      it "which has a timezone" do
-        expect(subject().timezone) |> to(eq("UTC"))
-      end
+      subject do: (Account.get_account(client(), 123456) |> elem(1))
+      it_behaves_like(SharedAccount)
     end
   end
 end
