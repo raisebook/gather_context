68badfde1e89ab1da2f8320c27b01cef65ccf42b
Myles Eftos <myles@madpilot.com.au>
2021-08-22 21:33:58 +1000

    Testing strcuture expansion in Item module

--- lib/gather_context/types/v2/item.ex
+++ lib/gather_context/types/v2/item.ex
@@ -1,12 +1,20 @@
 defmodule GatherContext.Types.V2.Item do
-  alias GatherContext.Types.V2.Structure
+  alias GatherContext.Types.V2.{Item, Structure}
 
-  defstruct id: nil,
-            project_id: nil,
-            name: nil,
+  defstruct name: nil,
             template_id: nil,
             structure: %Structure{},
             status_id: nil,
             folder_uuid: nil,
             position: 0
+
+  def encode(data = %Item{}) do
+    data
+    |> Map.from_struct()
+    |> Map.merge(%{
+      structure: data.structure |> Structure.encode()
+    })
+    |> Enum.filter(fn {_, v} -> v != nil end)
+    |> Enum.into(%{})
+  end
 end
